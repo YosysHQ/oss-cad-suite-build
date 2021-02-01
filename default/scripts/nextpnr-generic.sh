273f54ce877b21c1481a7d5c6bce3348ac557601
Miodrag Milanovic <mmicko@gmail.com>
2021-02-01 11:42:29 +0100

    No GUI for linux-arm

--- default/scripts/nextpnr-generic.sh
+++ default/scripts/nextpnr-generic.sh
@@ -1,6 +1,10 @@
 cd nextpnr
+build_gui="ON"
+if [ ${ARCH} == 'linux-arm' ]; then
+      build_gui="OFF"
+fi
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
       -DPYTHON_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.8 \
       -DPYTHON_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.8${SHARED_EXT} \
-      -DARCH=generic -DBUILD_GUI=ON .
+      -DARCH=generic -DBUILD_GUI=${build_gui} .
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
--- default/scripts/nextpnr-ice40.sh
+++ default/scripts/nextpnr-ice40.sh
@@ -1,7 +1,11 @@
 cd nextpnr
+build_gui="ON"
+if [ ${ARCH} == 'linux-arm' ]; then
+      build_gui="OFF"
+fi
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
       -DPYTHON_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.8 \
       -DPYTHON_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.8${SHARED_EXT} \
       -DARCH=ice40 -DICEBOX_DATADIR=${BUILD_DIR}/icestorm${INSTALL_PREFIX}/share/icebox \
-      -DBUILD_GUI=ON .
+      -DBUILD_GUI=${build_gui} .
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
