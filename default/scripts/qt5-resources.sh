cc59b8111ff004e738c722e3361db8f720829cc6
Miodrag Milanovic <mmicko@gmail.com>
2021-02-01 11:52:29 +0100

    Fixed typo

--- default/scripts/qt5-resources.sh
+++ default/scripts/qt5-resources.sh
@@ -1,4 +1,4 @@
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins
-if [ ${ARCH_BASE} == 'linux4' ]; then
+if [ ${ARCH_BASE} == 'linux' ]; then
     cp -rv /usr/lib/${CROSS_NAME}/qt5/plugins/* ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins/.
 fi
