a56a4635a7d5f030bc629131dc64fadcaee69a6c
Miodrag Milanovic <mmicko@gmail.com>
2021-02-03 15:55:14 +0100

    Added GHDL to Darwin, thanks to Ed Bordin

--- default/scripts/ghdl.sh
+++ default/scripts/ghdl.sh
@@ -1,4 +1,8 @@
 cd ghdl
+if [ ${ARCH_BASE} == 'darwin' ]; then
+    export PATH="$PATH:/opt/gnat/bin"
+    export GNAT_LARGS="-static-libgcc"
+fi
 ./configure --prefix=${INSTALL_PREFIX}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
