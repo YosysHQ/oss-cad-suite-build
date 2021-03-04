3e17381dbc17ec4dde299ff1ecc83a9bbf1254a9
Miodrag Milanovic <mmicko@gmail.com>
2021-03-04 16:56:59 +0100

    Make gtkwave compile for windows

--- default/scripts/gtkwave.sh
+++ default/scripts/gtkwave.sh
@@ -1,6 +1,8 @@
 cd gtkwave
 if [ ${ARCH_BASE} == 'darwin' ]; then
     ./configure --prefix=${INSTALL_PREFIX} --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.Framework
+elif [ ${ARCH_BASE} == 'windows' ]; then
+    ./configure --prefix=${INSTALL_PREFIX} --with-tcl=/mingw64/lib --with-tk=/mingw64/lib
 else
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
 fi
