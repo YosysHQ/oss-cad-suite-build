36c15d4dda2e88ab1524e6c1b3beefeebdc72712
Miodrag Milanovic <mmicko@gmail.com>
2021-03-04 17:02:14 +0100

    Building avy for Windows

--- default/scripts/avy.sh
+++ default/scripts/avy.sh
@@ -2,7 +2,13 @@ cd avy
 patch -p1 < ${PATCHES_DIR}/avy.diff
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+if [ ${ARCH_BASE} == 'windows' ]; then
+    sed -i -re "s,m dl pthread z,m pthread z,g" ../avy/src/CMakeLists.txt
+    cmake .. -DCMAKE_BUILD_TYPE=Release -D CMAKE_CXX_FLAGS="-DABC_USE_STDINT_H -DWIN32_NO_DLL -DHAVE_STRUCT_TIMESPEC -fpermissive -w"
+else
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+fi
+ 
 make -j${NPROC}
 mkdir -p ${OUTPUT_DIR}/${INSTALL_PREFIX}/bin
 ${STRIP} avy/src/{avy${EXE},avybmc${EXE}}
