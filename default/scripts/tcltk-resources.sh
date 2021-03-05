0b8e4516215817eb31de053949ab80d65039da3f
Miodrag Milanovic <mmicko@gmail.com>
2021-03-05 13:49:53 +0100

    Proper windows location

--- default/scripts/tcltk-resources.sh
+++ default/scripts/tcltk-resources.sh
@@ -5,6 +5,10 @@ if [ ${ARCH_BASE} == 'darwin' ]; then
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
     cp -r $(brew --prefix tcl-tk)/lib/tcl8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tcl8.6
     cp -r $(brew --prefix tcl-tk)/lib/tk8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tk8.6
+elif [ ${ARCH_BASE} == 'windows' ]; then
+	mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
+	cp -r /mingw64/lib/tcl8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tcl8.6
+	cp -r /mingw64/lib/tk8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tk8.6
 else
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
     cp -r /usr/share/tcltk/tcl8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tcl8.6
