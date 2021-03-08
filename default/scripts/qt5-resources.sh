363346abe99785dc85a9a97bbeb9afc2fb9237a9
Miodrag Milanovic <mmicko@gmail.com>
2021-03-08 09:13:49 +0100

    Add qt5 plugins for windows

--- default/scripts/qt5-resources.sh
+++ default/scripts/qt5-resources.sh
@@ -5,3 +5,6 @@ mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins
 if [ ${ARCH_BASE} == 'linux' ]; then
     cp -rv /usr/lib/${CROSS_NAME}/qt5/plugins/* ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins/.
 fi
+if [ ${ARCH_BASE} == 'windows' ]; then
+    cp -rv /mingw64/share/qt5/plugins/* ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins/.
+fi
