05b6f9a82c7944734985254bed74b6ebd7345304
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 16:19:26 +0100

    Fix for windows cross compile

--- default/scripts/z3.sh
+++ default/scripts/z3.sh
@@ -1,5 +1,9 @@
 cd z3
 if [ ${ARCH_BASE} == 'windows' ]; then
+	if [ ${IS_NATIVE} != 'True' ]; then
+		sed -i 's,IS_LINUX=True,IS_MSYS2=True,g' scripts/mk_util.py
+		sed -i 's/sysname, _, _, _, machine = os.uname()/sysname,machine=[ "MINGW64", "x86_64"]/g' scripts/mk_util.py
+	fi
 	/usr/bin/python3 scripts/mk_make.py
 else
 	python scripts/mk_make.py
