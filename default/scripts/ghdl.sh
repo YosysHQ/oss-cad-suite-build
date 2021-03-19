c61f8bc8782562fe042c7df212094a80c0d908d3
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 16:22:15 +0100

    GHDL not possible to cross compile for now

--- default/scripts/ghdl.sh
+++ default/scripts/ghdl.sh
@@ -2,6 +2,11 @@ cd ghdl
 if [ ${ARCH_BASE} == 'darwin' ]; then
     export PATH="$PATH:/opt/gnat/bin"
     export GNAT_LARGS="-static-libgcc"
+elif [ ${ARCH_BASE} == 'windows' ]; then
+    if [ ${IS_NATIVE} != 'True' ]; then
+        echo "GHDL not possible for Windows cross-compile for now."
+        exit
+    fi
 fi
 ./configure --prefix=${INSTALL_PREFIX}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC}
