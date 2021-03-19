abeb05ff089a2278c6fee71be3078618dd84f8df
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 17:05:13 +0100

    Packaging for windows cross compile

--- default/scripts/default.sh
+++ default/scripts/default.sh
@@ -219,32 +219,63 @@ done
 # end of Darwin/macOS section
 elif [ ${ARCH_BASE} == 'windows' ]; then
 # Windows section
-for bindir in bin py3bin; do
-	for f in `ntldd -R ${OUTPUT_DIR}${INSTALL_PREFIX}/$bindir/*.exe | grep mingw64 | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'`; do
-		cp -v "$f" lib/.
-	done
-done
-
-for libdir in lib; do
-    for libfile in $(find $libdir -type f | xargs file | grep DLL | cut -f1 -d:); do
-        for lib in $(ntldd -R $libfile | grep mingw64 | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'); do
-            cp "${lib}" lib/.
+if [ ${IS_NATIVE} == 'True' ]; then
+    for bindir in bin py3bin; do
+        for f in `ntldd -R ${OUTPUT_DIR}${INSTALL_PREFIX}/$bindir/*.exe | grep mingw64 | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'`; do
+            cp -v "$f" lib/.
         done
     done
+
+    for libdir in lib; do
+        for libfile in $(find $libdir -type f | xargs file | grep DLL | cut -f1 -d:); do
+            for lib in $(ntldd -R $libfile | grep mingw64 | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'); do
+                cp "${lib}" lib/.
+            done
+        done
+    done
+else
+    for bindir in bin py3bin; do
+        for binfile in $(file -h $bindir/* | grep PE32 | grep executable | cut -f1 -d:); do
+            for f in `peldd --all $binfile --wlist uxtheme.dll --wlist userenv.dll --wlist opengl32.dll | grep sys-root | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'`; do
+                cp -v "$f" lib/.
+            done
+        done
+    done
+
+    for libdir in lib; do
+        for libfile in $(find $libdir -type f | xargs file | grep DLL | cut -f1 -d:); do
+            for lib in $(peldd --all $libfile --wlist uxtheme.dll --wlist userenv.dll --wlist opengl32.dll | grep sys-root | sed -e 's/.*=..//' | sed -e 's/ (0.*)//'); do
+                cp "${lib}" lib/.
+            done
+        done
+    done
+fi
+
+for script in bin/* py3bin/*; do
+    if $(head -1 "${script}" | grep -q python); then
+		if [[ $script == *-script.py ]]; then
+			${CC} -DGUI=0 -O -s -o ${script/-script.py/.exe} ${PATCHES_DIR}/win-launcher.c
+		elif [[ $script == */icebox.py ]]; then
+			echo "Ignore icebox.py"
+		else
+			cp ${script} ${script/.py/}-script.py
+			${CC} -DGUI=0 -O -s -o ${script/.py/}.exe ${PATCHES_DIR}/win-launcher.c
+		fi
+    fi
 done
 
 for script in bin/* py3bin/*; do
     if $(head -1 "${script}" | grep -q python); then
 		if [[ $script == *-script.py ]]; then
-			gcc -DGUI=0 -O -s -o ${script/-script.py/.exe} ${PATCHES_DIR}/win-launcher.c
+			echo "Ignore $script"
 		elif [[ $script == */icebox.py ]]; then
 			echo "Ignore icebox.py"
 		else
-			mv ${script} ${script/.py/}-script.py
-			gcc -DGUI=0 -O -s -o ${script/.py/}.exe ${PATCHES_DIR}/win-launcher.c
+			rm -rf ${script}
 		fi
     fi
 done
+${CC} -DGUI=0 -O -s -o bin/yosys-smtbmc.exe ${PATCHES_DIR}/win-launcher.c
 # end of Windows section
 fi
 
