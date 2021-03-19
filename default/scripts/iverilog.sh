12d465c2825d6c7df701f07dbe761eec5277e793
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 16:19:39 +0100

    Fix for windows cross compile

--- default/scripts/iverilog.sh
+++ default/scripts/iverilog.sh
@@ -1,4 +1,11 @@
 cd iverilog
+if [ ${ARCH_BASE} == 'windows' ]; then
+    if [ ${IS_NATIVE} != 'True' ]; then
+        sed -i "s,\$(bindir)/iverilog-vpi,\$(DESTDIR)\$(bindir)/iverilog-vpi,g" driver-vpi/Makefile.in 
+        sed -i "s,\$(srcdir)/../mkinstalldirs \"\$(bindir)\",\$(srcdir)/../mkinstalldirs \"\$(DESTDIR)\$(bindir)\",g" driver-vpi/Makefile.in
+        sed -i "s/\$(CC) \$(CFLAGS) -o draw_tt/gcc \$(CFLAGS) -o draw_tt/g" vvp/Makefile.in
+    fi
+fi
 sh autoconf.sh
 ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
