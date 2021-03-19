d797c2f0d9b9dea918969bc06956db1feffdc9fb
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 16:19:51 +0100

    Fix for windows cross compile

--- default/scripts/yices.sh
+++ default/scripts/yices.sh
@@ -5,7 +5,13 @@ if [ ${ARCH_BASE} == 'linux' ]; then
 	sed -i 's,make.include.\$(ARCH),make.include.${CROSS_NAME},g' Makefile
 fi
 if [ ${ARCH_BASE} == 'windows' ]; then
-	mv configs/make.include.x86_64-w64-mingw32 configs/make.include.x86_64-pc-mingw64
+    if [ ${IS_NATIVE} == 'True' ]; then
+		mv configs/make.include.x86_64-w64-mingw32 configs/make.include.x86_64-pc-mingw64
+	else	
+		sed -i 's,make.include.\$(ARCH),make.include.x86_64-w64-mingw32,g' Makefile
+		sed -i 's,ARCH=\$(shell ./config.sub `./config.guess`),ARCH=x86_64-pc-mingw64,g' Makefile
+		sed -i 's,POSIXOS=\$(shell ./autoconf/os),POSIXOS=mingw,g' Makefile
+	fi
 fi
 make DESTDIR=${OUTPUT_DIR} -j${NPROC}
 make DESTDIR=${OUTPUT_DIR} install
