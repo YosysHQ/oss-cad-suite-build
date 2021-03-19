09d2c606369cc0993411082ed03b1115ed136b56
Miodrag Milanovic <mmicko@gmail.com>
2021-03-19 16:43:34 +0100

    Package mingw from cross compile environment

--- default/scripts/python3.sh
+++ default/scripts/python3.sh
@@ -1,15 +1,32 @@
 if [ ${LOCAL_PYTHON} == 'True' ]; then
+	mkdir -p python3
+	cd python3
+	curl -L https://raw.githubusercontent.com/python/cpython/master/LICENSE > LICENSE
 	if [ ${ARCH} == 'windows-x64' ]; then
-		for f in $(pacman -Ql mingw-w64-x86_64-python mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-setuptools | cut -f2 -d' '); do
-			if [ -d "$f" ]; then
-				mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/$f
-			elif [ -f "$f" ]; then
-				cp $f ${OUTPUT_DIR}${INSTALL_PREFIX}/$f
-			fi
-		done
-		mv ${OUTPUT_DIR}${INSTALL_PREFIX}/mingw64/* ${OUTPUT_DIR}${INSTALL_PREFIX}/.
-		rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/mingw64
-		mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/py3bin
+		if [ ${IS_NATIVE} == 'True' ]; then
+			for f in $(pacman -Ql mingw-w64-x86_64-python mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-setuptools | cut -f2 -d' '); do
+				if [ -d "$f" ]; then
+					mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/$f
+				elif [ -f "$f" ]; then
+					cp $f ${OUTPUT_DIR}${INSTALL_PREFIX}/$f
+				fi
+			done
+			mv ${OUTPUT_DIR}${INSTALL_PREFIX}/mingw64/* ${OUTPUT_DIR}${INSTALL_PREFIX}/.
+			rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/mingw64
+			mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/py3bin
+		else
+			for f in $(dnf repoquery --installed -l mingw64-python3 | grep /usr/x86_64-w64-mingw32/sys-root/mingw); do
+				if [ -d "$f" ]; then
+					mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}$f
+				elif [ -f "$f" ]; then
+					mkdir -p `dirname ${OUTPUT_DIR}${INSTALL_PREFIX}$f`
+					cp -r $f ${OUTPUT_DIR}${INSTALL_PREFIX}$f
+				fi
+			done
+			mv ${OUTPUT_DIR}${INSTALL_PREFIX}/usr/x86_64-w64-mingw32/sys-root/mingw/* ${OUTPUT_DIR}${INSTALL_PREFIX}/.
+			rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/usr
+			mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/py3bin
+		fi		
 	fi
     exit
 fi
