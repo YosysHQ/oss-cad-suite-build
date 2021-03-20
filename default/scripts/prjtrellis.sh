d6e296cceec013f71f9247d397954fc1f49550fa
Miodrag Milanovic <mmicko@gmail.com>
2021-03-20 13:49:47 +0100

    Make prjtrellis cross compile for windows

--- default/scripts/prjtrellis.sh
+++ default/scripts/prjtrellis.sh
@@ -4,6 +4,12 @@ if [ ${LOCAL_PYTHON} != 'True' ]; then
       python_var="-DPYTHON_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.8
                   -DPYTHON_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.8${SHARED_EXT}"
 fi
+if [ ${ARCH_BASE} == 'windows' ]; then
+    if [ ${IS_NATIVE} != 'True' ]; then
+        sed -i 's/Windows.h/windows.h/g' tools/ecpbram.cpp
+        sed -i 's,\${CMAKE_DL_LIBS}, ,g' CMakeLists.txt
+    fi
+fi
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
       ${python_var} .
