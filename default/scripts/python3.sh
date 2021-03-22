6a6fbb50aa8326a0437ca199855a18f3d260d1a6
Miodrag Milanovic <mmicko@gmail.com>
2021-03-22 12:06:57 +0100

    Proper python3 cross compile

--- default/scripts/python3.sh
+++ default/scripts/python3.sh
@@ -39,10 +39,16 @@ if [ ${ARCH} == 'darwin-x64' ]; then
     export LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix libffi)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix openssl)/lib"
     ./configure --prefix=${INSTALL_PREFIX} --enable-optimizations --enable-shared --with-system-ffi --with-openssl=$(brew --prefix openssl)
 else
-    ./configure --prefix=${INSTALL_PREFIX} --enable-optimizations --enable-shared --with-system-ffi
+    echo "ac_cv_file__dev_ptmx=no" > config.site
+    echo "ac_cv_file__dev_ptc=no" >> config.site
+    CONFIG_SITE=config.site ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --build=`gcc -dumpmachine` --disable-ipv6 --enable-optimizations --enable-shared --with-system-ffi --with-ensurepip=install
 fi
 
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
+if [ -d "${OUTPUT_DIR}/usr" ]; then
+    cp -r ${OUTPUT_DIR}/usr/* ${OUTPUT_DIR}${INSTALL_PREFIX}/.
+    rm -rf ${OUTPUT_DIR}/usr
+fi
 mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/py3bin
 if [ ${ARCH_BASE} == 'darwin' ]; then
     install_name_tool -id ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/libpython3.8.dylib ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/libpython3.8.dylib
