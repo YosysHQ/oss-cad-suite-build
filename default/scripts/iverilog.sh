fa0fca3e39c492dbf8938f6bfcd3cb584f7c7ce5
Miodrag Milanovic <mmicko@gmail.com>
2021-03-22 12:36:56 +0100

    Proper cross compile

--- default/scripts/iverilog.sh
+++ default/scripts/iverilog.sh
@@ -1,9 +1,9 @@
 cd iverilog
+sed -i "s/\$(CC) \$(CFLAGS) -o draw_tt/gcc \$(CFLAGS) -o draw_tt/g" vvp/Makefile.in
 if [ ${ARCH_BASE} == 'windows' ]; then
     if [ ${IS_NATIVE} != 'True' ]; then
         sed -i "s,\$(bindir)/iverilog-vpi,\$(DESTDIR)\$(bindir)/iverilog-vpi,g" driver-vpi/Makefile.in 
         sed -i "s,\$(srcdir)/../mkinstalldirs \"\$(bindir)\",\$(srcdir)/../mkinstalldirs \"\$(DESTDIR)\$(bindir)\",g" driver-vpi/Makefile.in
-        sed -i "s/\$(CC) \$(CFLAGS) -o draw_tt/gcc \$(CFLAGS) -o draw_tt/g" vvp/Makefile.in
     fi
 fi
 sh autoconf.sh
