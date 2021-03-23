6456678f4d4171d3f1b212bfa9c3f30950605e4e
Miodrag Milanovic <mmicko@gmail.com>
2021-03-23 16:50:30 +0100

    only useful when cross compile

--- default/scripts/nextpnr-bba.sh
+++ default/scripts/nextpnr-bba.sh
@@ -1,7 +1,9 @@
-cd nextpnr/bba
-cmake -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON
-make -j${NPROC}
-sed -i 's,'${BUILD_DIR}','${OUTPUT_DIR}',g' bba-export.cmake
-mkdir -p ${OUTPUT_DIR}/nextpnr/bba
-cp bbasm ${OUTPUT_DIR}/nextpnr/bba/.
-cp bba-export.cmake ${OUTPUT_DIR}/nextpnr/bba/.
+if [ ${ARCH} == 'linux-x64' ]; then
+    cd nextpnr/bba
+    cmake -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON
+    make -j${NPROC}
+    sed -i 's,'${BUILD_DIR}','${OUTPUT_DIR}',g' bba-export.cmake
+    mkdir -p ${OUTPUT_DIR}/nextpnr/bba
+    cp bbasm ${OUTPUT_DIR}/nextpnr/bba/.
+    cp bba-export.cmake ${OUTPUT_DIR}/nextpnr/bba/.
+fi
