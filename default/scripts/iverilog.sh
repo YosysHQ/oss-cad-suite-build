9c1f9195b61cb3d41000c768183ab9cceb63a97b
Miodrag Milanovic <mmicko@gmail.com>
2021-03-25 12:33:49 +0100

    cleanup

--- default/scripts/iverilog.sh
+++ default/scripts/iverilog.sh
@@ -1,5 +1,5 @@
 cd iverilog
 sed -i "s/\$(CC) \$(CFLAGS) -o draw_tt/gcc \$(CFLAGS) -o draw_tt/g" vvp/Makefile.in
 sh autoconf.sh
-./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --build=`gcc -dumpmachine`
+./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
