b89ec4b4d4c1540500818aa2d0b0bed03d3d82cc
Miodrag Milanovic <mmicko@gmail.com>
2021-03-31 17:14:43 +0200

    cleanup

--- default/scripts/pono.sh
+++ default/scripts/pono.sh
@@ -9,16 +9,8 @@ cd pono
 sed -i -re 's,./configure,./configure --host=\$\{CROSS_NAME\},g' contrib/setup-bison.sh
 sed -i -re 's,cmake \"$root_dir\" \$cmake_opts,cmake \"$root_dir\" \$cmake_opts -DCMAKE_TOOLCHAIN_FILE=\$\{CMAKE_TOOLCHAIN_FILE\},g' CMakeLists.txt 
 sed -i 's,target_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/libsmt-switch-btor.a"),target_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/libsmt-switch-btor.a")\ntarget_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/liblgl.a"),g' CMakeLists.txt 
-./configure.sh
+./configure.sh --static
 cd build
 make -j${NPROC}
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/bin
-mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
 cp pono ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/.
-if [ ${ARCH_BASE} == 'linux' ]; then
-    cp libpono.so ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-fi
-if [ ${ARCH_BASE} == 'darwin' ]; then
-    cp libpono.dylib ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    install_name_tool -add_rpath ${OUTPUT_DIR}${INSTALL_PREFIX}/lib ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/pono
-fi
