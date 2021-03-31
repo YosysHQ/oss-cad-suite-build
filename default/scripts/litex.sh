1dcfc766aeea4d798b3356a9a6415c5d467fe074
Miodrag Milanovic <mmicko@gmail.com>
2021-03-31 09:57:29 +0200

    Need to install as develop in order to work

--- default/scripts/litex.sh
+++ default/scripts/litex.sh
@@ -1,5 +1,5 @@
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/bin
-mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages
+mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/litex
 export _PYTHON_PROJECT_BASE=${BUILD_DIR}/python3${INSTALL_PREFIX}
 export PYTHONHOME=${BUILD_DIR}/python3${INSTALL_PREFIX}
 export PYTHONPATH=${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages:${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/python3.8:${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/python3.8/site-packages
@@ -18,23 +18,29 @@ elif [ ${ARCH} == 'windows-x64' ]; then
 	cp -r ${BUILD_DIR}/python3${INSTALL_PREFIX}/lib ${BUILD_DIR}/python3${INSTALL_PREFIX}/lib64
 fi
 
-install_cmd="setup.py install --prefix=${OUTPUT_DIR}${INSTALL_PREFIX}"
-for target in *; do
-    pushd $target
+for target in *; do    
     if [ $target != 'python3' ]; then
-        if [ ${ARCH} == 'linux-arm' ]; then
-            _PYTHON_HOST_PLATFORM=linux-arm  _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_arm-linux-gnueabihf python3.8 ${install_cmd}
-        elif [ ${ARCH} == 'linux-arm64' ]; then
-            _PYTHON_HOST_PLATFORM=linux-aarch64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_aarch64-linux-gnu python3.8 ${install_cmd}
-        elif [ ${ARCH} == 'linux-riscv64' ]; then
-            _PYTHON_HOST_PLATFORM=linux-riscv64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_riscv64-linux-gnu python3.8 ${install_cmd}
-        elif [ ${ARCH} == 'linux-x64' ]; then
-            ${PYTHONHOME}/py3bin/python3 ${install_cmd}
-        elif [ ${ARCH} == 'windows-x64' ]; then
-            _PYTHON_HOST_PLATFORM=mingw-x64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__win_ python3.8 ${install_cmd}
-        elif [ ${ARCH} == 'darwin-x64' ]; then
-            DYLD_LIBRARY_PATH=${PYTHONHOME}/lib ${PYTHONHOME}/py3bin/python3 ${install_cmd}
-        fi
+        cp -r $target ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/litex/.
+    fi
+done
+
+cd ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/litex
+for target in *; do
+    install_cmd="setup.py develop --prefix=${OUTPUT_DIR}${INSTALL_PREFIX} --egg-path ./litex/$target"
+    pushd $target
+    if [ ${ARCH} == 'linux-arm' ]; then
+        _PYTHON_HOST_PLATFORM=linux-arm  _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_arm-linux-gnueabihf python3.8 ${install_cmd}
+    elif [ ${ARCH} == 'linux-arm64' ]; then
+        _PYTHON_HOST_PLATFORM=linux-aarch64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_aarch64-linux-gnu python3.8 ${install_cmd}
+    elif [ ${ARCH} == 'linux-riscv64' ]; then
+        _PYTHON_HOST_PLATFORM=linux-riscv64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__linux_riscv64-linux-gnu python3.8 ${install_cmd}
+    elif [ ${ARCH} == 'linux-x64' ]; then
+        ${PYTHONHOME}/py3bin/python3 ${install_cmd}
+    elif [ ${ARCH} == 'windows-x64' ]; then
+        _PYTHON_HOST_PLATFORM=mingw-x64 _PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__win_ python3.8 ${install_cmd}
+    elif [ ${ARCH} == 'darwin-x64' ]; then
+        DYLD_LIBRARY_PATH=${PYTHONHOME}/lib ${PYTHONHOME}/py3bin/python3 ${install_cmd}
     fi
     popd
 done
+( find . -type d -name ".git" && find . -name ".gitignore" && find . -name ".gitmodules" && find . -name ".gitattributes" && find . -name ".github" ) | xargs rm -rf
