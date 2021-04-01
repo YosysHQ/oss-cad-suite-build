f21c292660e2c28c135ecee0aa4c01fc7cb40237
Miodrag Milanovic <mmicko@gmail.com>
2021-04-01 15:58:56 +0200

    make it work on each machine

--- default/scripts/nextpnr-bba.sh
+++ default/scripts/nextpnr-bba.sh
@@ -1,9 +1,7 @@
-if [ ${ARCH} == 'linux-x64' ]; then
-    cd nextpnr/bba
-    cmake -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON
-    make -j${NPROC}
-    sed -i 's,'${BUILD_DIR}','${OUTPUT_DIR}',g' bba-export.cmake
-    mkdir -p ${OUTPUT_DIR}/nextpnr/bba
-    cp bbasm ${OUTPUT_DIR}/nextpnr/bba/.
-    cp bba-export.cmake ${OUTPUT_DIR}/nextpnr/bba/.
-fi
+cd nextpnr/bba
+cmake -DCMAKE_BUILD_TYPE=Release -DBoost_USE_STATIC_LIBS=ON -DCMAKE_CXX_STANDARD=14 
+make -j${NPROC}
+sed -i 's,'${BUILD_DIR}','${OUTPUT_DIR}',g' bba-export.cmake
+mkdir -p ${OUTPUT_DIR}/nextpnr/bba
+cp bbasm ${OUTPUT_DIR}/nextpnr/bba/.
+cp bba-export.cmake ${OUTPUT_DIR}/nextpnr/bba/.
