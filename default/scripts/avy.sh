481c93ed51d98ac497ce431956835365fba9cdb0
Miodrag Milanovic <mmicko@gmail.com>
2021-04-08 13:05:26 +0200

    No need for this patch

--- default/scripts/avy.sh
+++ default/scripts/avy.sh
@@ -3,7 +3,6 @@ patch -p1 < ${PATCHES_DIR}/avy.diff
 mkdir -p build
 cd build
 if [ ${ARCH_BASE} == 'windows' ]; then
-    sed -i -re "s,m dl pthread z,m pthread z,g" ../avy/src/CMakeLists.txt
     cmake .. -DCMAKE_BUILD_TYPE=Release -D CMAKE_CXX_FLAGS="-DABC_USE_STDINT_H -DWIN32_NO_DLL -DHAVE_STRUCT_TIMESPEC -fpermissive -w" -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
 else
     cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
