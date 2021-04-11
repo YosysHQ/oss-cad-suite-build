44cef63ef216b1bab6ad9598e5c8c6d92a517abd
Miodrag Milanovic <mmicko@gmail.com>
2021-04-11 16:36:54 +0200

    Fix examples

--- default/scripts/sby.sh
+++ default/scripts/sby.sh
@@ -1,4 +1,4 @@
 cd sby
 make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} -j${NPROC} install
-mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/examples
-cp -r docs/examples/* ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/.
+mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/sby
+cp -r docs/examples/* ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/sby/.
