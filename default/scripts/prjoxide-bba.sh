4f99e209a3a6f11d0f7ca6d2928aae1014672f4f
Miodrag Milanovic <mmicko@gmail.com>
2021-04-13 18:11:09 +0200

    revert to be able to build bba

--- default/scripts/prjoxide-bba.sh
+++ default/scripts/prjoxide-bba.sh
@@ -1,4 +1,5 @@
 pushd nextpnr/nexus
+cp ${BUILD_DIR}/prjoxide/packages/prjoxide/libexec/prjoxide ${BUILD_DIR}/prjoxide/packages/prjoxide/bin/prjoxide
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
       -DOXIDE_INSTALL_PREFIX=${BUILD_DIR}/prjoxide/packages/prjoxide \
       .
