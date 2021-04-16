1df16f72472011a180312a3fc887ef5c67179a3d
Miodrag Milanovic <mmicko@gmail.com>
2021-04-16 10:46:06 +0200

    remove .py from some executable scripts

--- default/scripts/python-programmers.sh
+++ default/scripts/python-programmers.sh
@@ -17,3 +17,7 @@ done
 python3_package_pth "python-programmers"
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/tqdm
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/jsonschema
+for f in ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/*.py
+do
+    [ -f "$f" ] && mv "$f" "${f%.py}"
+done
