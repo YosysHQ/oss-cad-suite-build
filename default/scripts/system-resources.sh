5c69459c183f965b8cb1561d0f12b4c203087a9f
Miodrag Milanovic <mmicko@gmail.com>
2021-05-07 10:00:14 +0200

    do not duplicate

--- default/scripts/system-resources.sh
+++ default/scripts/system-resources.sh
@@ -3,16 +3,6 @@ mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins
 
 if [ ${ARCH_BASE} == 'linux' ]; then
     # Linux section
-    if [ ${ARCH} == 'linux-x64' ]; then
-        arch_prefix="x86_64-linux-gnu"
-    elif [ ${ARCH} == 'linux-arm64' ]; then
-        arch_prefix="aarch64-linux-gnu"
-    elif [ ${ARCH} == 'linux-arm' ]; then
-        arch_prefix="arm-linux-gnueabihf"
-    elif [ ${ARCH} == 'linux-riscv64' ]; then
-        arch_prefix="riscv64-linux-gnu"
-    fi
-    
     cp ${PATCHES_DIR}/environment ${OUTPUT_DIR}${INSTALL_PREFIX}/.
 
     cp -r /usr/share/fonts/. ${OUTPUT_DIR}${INSTALL_PREFIX}/share/fonts
@@ -23,40 +13,14 @@ if [ ${ARCH_BASE} == 'linux' ]; then
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
     cp -r /usr/share/tcltk/tcl8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tcl8.6
     cp -r /usr/share/tcltk/tk8.6/. ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/tk8.6
-    cp -rL /usr/lib/${CROSS_NAME}/libtcl8.6.so ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libtk8.6.so ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
 
     cp -rv /usr/lib/${CROSS_NAME}/qt5/plugins/* ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/qt5/plugins/.
-    cp -rvL /usr/lib/${CROSS_NAME}/libQt5*.so.5 ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
+
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/gtk-2.0/modules
     cp -v `pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0`/* ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
     cp -v `pkg-config --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0` ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/gtk-2.0/.
     sed -i -re "s,$(pkg-config --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)/,,g" ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/gtk-2.0/loaders.cache
     touch ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/gtk-2.0/gtkrc
-    cp -rvL /usr/lib/${CROSS_NAME}/libgtk*.so.0 ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    
-    cp -rL /usr/lib/${CROSS_NAME}/libboost_*.so.1.*  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libftdi1.so.2  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libhidapi-*.so.0  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libreadline.so.8  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libutil.so.1  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libcrypt.so.1  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libssl.so.1.1  ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libpanelw.so.6 ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-    cp -rL /usr/lib/${CROSS_NAME}/libncursesw.so.6 ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-
-    for lib in /lib/$arch_prefix/libnss_dns.so.2 /lib/$arch_prefix/libnss_files.so.2 /lib/$arch_prefix/libnss_compat.so.2 /lib/$arch_prefix/libresolv.so.2 \
-            /lib/$arch_prefix/libnss_nis.so.2 /lib/$arch_prefix/libnss_nisplus.so.2 /lib/$arch_prefix/libnss_hesiod.so.2 /lib/$arch_prefix/libnsl.so.1; do
-        cp "${lib}" ${OUTPUT_DIR}${INSTALL_PREFIX}/lib
-    done
-
-    for libdir in ${OUTPUT_DIR}${INSTALL_PREFIX}/lib; do
-        for libfile in $(find $libdir -type f | xargs file | grep ELF | grep dynamically | cut -f1 -d:); do
-            for lib in $(lddtree -l $libfile | tail -n +2 | grep ^/ ); do
-                cp -i "${lib}" ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/.
-            done
-        done
-    done
 fi
 
 if [ ${ARCH_BASE} == 'darwin' ]; then
