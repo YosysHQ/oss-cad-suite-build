dce9d28cffa1d764b2d1aa1b545a2a68fe7fbe9a
Miodrag Milanovic <mmicko@gmail.com>
2021-05-13 17:17:09 +0200

    Fix yices

--- default/scripts/yices.sh
+++ default/scripts/yices.sh
@@ -10,6 +10,10 @@ autoconf
 if [ ${ARCH_BASE} == 'linux' ]; then
 	sed -i 's,make.include.\$(ARCH),make.include.${CROSS_NAME},g' Makefile
 fi
+if [ ${ARCH_BASE} == 'darwin' ] ; then
+	sed -i 's,make.include.\$(ARCH),make.include.${CROSS_NAME},g' Makefile
+	sed -i 's,POSIXOS=\$(shell ./autoconf/os),POSIXOS=darwin,g' Makefile
+fi
 if [ ${ARCH_BASE} == 'windows' ]; then
 	sed -i 's,make.include.\$(ARCH),make.include.x86_64-w64-mingw32,g' Makefile
 	sed -i 's,ARCH=\$(shell ./config.sub `./config.guess`),ARCH=x86_64-pc-mingw64,g' Makefile
