cdd486fdd08e51f7d101b396ade7367bfbeb6358
Miodrag Milanovic <mmicko@gmail.com>
2021-05-13 15:18:28 +0200

    proper tk framework location

--- default/scripts/gtkwave.sh
+++ default/scripts/gtkwave.sh
@@ -1,6 +1,6 @@
 cd gtkwave/gtkwave3
 if [ ${ARCH_BASE} == 'darwin' ]; then
-    ./configure --prefix=${INSTALL_PREFIX} --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.Framework
+    ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.framework
 elif [ ${ARCH_BASE} == 'windows' ]; then
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} MINGW_LDADD="-lcomdlg32" --with-tcl=/usr/x86_64-w64-mingw32/sys-root/mingw/lib --with-tk=/usr/x86_64-w64-mingw32/sys-root/mingw/lib
 else
