e711a3e269b823175fff729f199c113de38f7ba4
Miodrag Milanovic <mmicko@gmail.com>
2021-05-13 15:12:48 +0200

    Explicit target parameter for rust

--- default/scripts/prjoxide.sh
+++ default/scripts/prjoxide.sh
@@ -7,10 +7,11 @@ elif [ ${ARCH} == 'linux-arm64' ]; then
 elif [ ${ARCH} == 'linux-riscv64' ]; then
 	param="--target=riscv64gc-unknown-linux-gnu"
 elif [ ${ARCH} == 'linux-x64' ]; then
-    param=""
+    param="--target=x86_64-unknown-linux-gnu"
 elif [ ${ARCH} == 'darwin-x64' ]; then
-    param=""
+    param="--target x86_64-apple-darwin"
 elif [ ${ARCH} == 'windows-x64' ]; then
+    export PKG_CONFIG_SYSROOT_DIR=/usr/x86_64-w64-mingw32/sys-root/mingw/include
     param="--target=x86_64-pc-windows-gnu"
 fi
 cargo install --no-track --path prjoxide --root ${OUTPUT_DIR}${INSTALL_PREFIX} ${param}
