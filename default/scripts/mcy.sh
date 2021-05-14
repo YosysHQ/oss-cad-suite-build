d22e7e2a7763bf3b239759dd9b49338118323da8
Miodrag Milanovic <mmicko@gmail.com>
2021-05-14 12:13:55 +0200

    no need for build step

--- default/scripts/mcy.sh
+++ default/scripts/mcy.sh
@@ -1,6 +1,5 @@
 cd mcy
 sed -i -re "s,cmake,cmake -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_TOOLCHAIN_FILE},g" Makefile
-make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} -j${NPROC} build
 make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} -j${NPROC} install
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/mcy_demo
 cp -r examples/* ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/mcy_demo/.
