abd0be7e7d77872cd5c0834abee5b173670b52be
Miodrag Milanovic <mmicko@gmail.com>
2021-05-14 14:36:14 +0200

    yices fix for darwin

--- default/scripts/yices.sh
+++ default/scripts/yices.sh
@@ -6,13 +6,18 @@ if [ ${PACK_SOURCES} == 'True' ]; then
 	popd
 fi
 autoconf
-./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
+if [ ${ARCH_BASE} == 'darwin' ] ; then
+	./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} -with-static-gmp=/opt/local/lib/libgmp.a
+else
+	./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
+fi
 if [ ${ARCH_BASE} == 'linux' ]; then
 	sed -i 's,make.include.\$(ARCH),make.include.${CROSS_NAME},g' Makefile
 fi
 if [ ${ARCH_BASE} == 'darwin' ] ; then
 	sed -i 's,make.include.\$(ARCH),make.include.${CROSS_NAME},g' Makefile
 	sed -i 's,POSIXOS=\$(shell ./autoconf/os),POSIXOS=darwin,g' Makefile
+	sed -i 's,/usr/bin/mktemp -t out,mktemp,g' utils/make_source_version
 fi
 if [ ${ARCH_BASE} == 'windows' ]; then
 	sed -i 's,make.include.\$(ARCH),make.include.x86_64-w64-mingw32,g' Makefile
