cb87dc49d36cd53e1dda3ba2521aacc213e1c55e
Miodrag Milanovic <mmicko@gmail.com>
2021-05-14 12:35:39 +0200

    z3 cross compile

--- default/scripts/z3.sh
+++ default/scripts/z3.sh
@@ -3,6 +3,10 @@ if [ ${ARCH_BASE} == 'windows' ]; then
 	sed -i 's,IS_LINUX=True,IS_MSYS2=True,g' scripts/mk_util.py
 	sed -i 's/sysname, _, _, _, machine = os.uname()/sysname,machine=[ "MINGW64", "x86_64"]/g' scripts/mk_util.py
 fi
+if [ ${ARCH_BASE} == 'darwin' ]; then
+	sed -i 's,IS_LINUX=True,IS_OSX=True,g' scripts/mk_util.py
+	sed -i 's/sysname, _, _, _, machine = os.uname()/sysname,machine=[ "Darwin", "x86_64"]/g' scripts/mk_util.py
+fi
 python3 scripts/mk_make.py
 cd build
 if [ ${ARCH} == 'linux-riscv64' ]; then
