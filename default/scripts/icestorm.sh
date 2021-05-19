6b16eafac0e3d6f90181d663a038202d7e4feb3a
Miodrag Milanovic <mmicko@gmail.com>
2021-05-19 08:23:48 +0200

    Cleanup

--- default/scripts/icestorm.sh
+++ default/scripts/icestorm.sh
@@ -4,4 +4,6 @@ if [ ${ARCH_BASE} != 'windows' ]; then
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec
     mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/icebox.py ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec/.
     mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/iceboxdb.py ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec/.
+else
+    sed 's|#!/usr/bin/env python3|#|g' -i  ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/icebox.py
 fi
--- scripts/package-windows.sh
+++ scripts/package-windows.sh
@@ -1,23 +1,25 @@
 cd ${OUTPUT_DIR}${INSTALL_PREFIX}
 mkdir -p lib
-mkdir -p libexec
 
 rm -rf ${OUTPUT_DIR}/dev
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/include
 
 sed "s|___BRANDING___|${BRANDING}|g" -i ${OUTPUT_DIR}${INSTALL_PREFIX}/environment.bat
 
+# We copy all DLLs that we are using
 cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll lib/.
+
+# Replace symbolic links with actual files
 for f in $(find . -type l)
 do
     cp --remove-destination $(readlink -e $f) $f
 done
+
+# Replace python scripts with python launchers
 for script in bin/* py3bin/*; do
     if $(head -1 "${script}" | grep -q python); then
 		if [[ $script == *-script.py ]]; then
 			cp ${OUTPUT_DIR}${INSTALL_PREFIX}/win-launcher.exe ${script/-script.py/.exe}
-		elif [[ $script == */icebox.py ]]; then
-			echo "Ignore icebox.py"
 		else
 			mv ${script} ${script/.py/}-script.py
 			cp ${OUTPUT_DIR}${INSTALL_PREFIX}/win-launcher.exe ${script/.py/}.exe
@@ -25,6 +27,7 @@ for script in bin/* py3bin/*; do
     fi
 done
 
+# Remove general purpose launcher
 rm ${OUTPUT_DIR}${INSTALL_PREFIX}/win-launcher.exe
 
 chmod -R u=rwX,go=rX *
