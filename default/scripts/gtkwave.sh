d84b58e065a8aed70eceeede51ff14b2bc8610db
Miodrag Milanovic <mmicko@gmail.com>
2021-05-22 15:26:20 +0200

    gtk3 build for gtkwave is broken on macos

--- default/scripts/gtkwave.sh
+++ default/scripts/gtkwave.sh
@@ -1,10 +1,11 @@
-cd gtkwave/gtkwave3-gtk3
 if [ ${ARCH_BASE} == 'darwin' ]; then
-    sed 's|variable=target|variable=targets|g' -i configure
-    ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3 --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.framework
+    cd gtkwave/gtkwave3
+    ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.framework
 elif [ ${ARCH_BASE} == 'windows' ]; then
+    cd gtkwave/gtkwave3-gtk3
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3 MINGW_LDADD="-lcomdlg32" --with-tcl=/usr/x86_64-w64-mingw32/sys-root/mingw/lib --with-tk=/usr/x86_64-w64-mingw32/sys-root/mingw/lib
 else
+    cd gtkwave/gtkwave3-gtk3
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3
 fi
 make DESTDIR=${OUTPUT_DIR} UPDATE_DESKTOP_DATABASE=/bin/true -j${NPROC} install
