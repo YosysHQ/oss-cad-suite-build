6b15e0e77d5319cf28402d18cce054b18edf69f0
Miodrag Milanovic <mmicko@gmail.com>
2021-05-26 08:15:31 +0200

    disable tests building for pono

--- default/scripts/pono.sh
+++ default/scripts/pono.sh
@@ -9,6 +9,7 @@ cp boolector/dev/deps/cadical/build/libcadical.a pono/deps/smt-switch/local/lib/
 cd pono
 sed -i -re 's,cmake \"\$root_dir\" \$cmake_opts,cmake \"\$root_dir\" \$cmake_opts -DCMAKE_TOOLCHAIN_FILE=\$\{CMAKE_TOOLCHAIN_FILE\},g' configure.sh
 sed -i 's,target_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/libsmt-switch-btor.a"),target_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/libsmt-switch-btor.a")\ntarget_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/liblgl.a")\ntarget_link_libraries(pono-lib PUBLIC "${SMT_SWITCH_DIR}/local/lib/libcadical.a"),g' CMakeLists.txt 
+sed -i 's,add_subdirectory(tests),#add_subdirectory(tests),g' CMakeLists.txt 
 if [ ${ARCH_BASE} == 'windows' ]; then
     sed -i -re 's,FMT_USE_WINDOWS_H 1,FMT_USE_WINDOWS_H 0,g' contrib/fmt/format.h
     sed -i -re 's,SIGALRM,SIGABRT,g' pono.cpp
