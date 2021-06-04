31f241c3caa4d51eed0bb8cd0911b8cfa0f42d57
Miodrag Milanovic <mmicko@gmail.com>
2021-06-04 13:06:59 +0200

    No need for special handling

--- default/scripts/verilator.sh
+++ default/scripts/verilator.sh
@@ -7,13 +7,9 @@ autoconf
 make DESTDIR=${OUTPUT_DIR} -j${NPROC}
 make DESTDIR=${OUTPUT_DIR} prefix=${INSTALL_PREFIX} -j${NPROC} install
 
-if [ ${ARCH_BASE} != 'darwin' ]; then
-    sed -i 's,CXX = '${CXX}',CXX = g++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
-    sed -i 's,LINK = '${CXX}',LINK = g++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
-else
-    sed -i 's,CXX = '${CXX}',CXX = clang++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
-    sed -i 's,LINK = '${CXX}',LINK = clang++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
-fi
+sed -i 's,CXX = '${CXX}',CXX = g++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
+sed -i 's,LINK = '${CXX}',LINK = g++,g' ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/include/verilated.mk
+
 ${STRIP} ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/verilator_bin*
 ${STRIP} ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/verilator_coverage_bin*
 cp ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/*_bin* ${OUTPUT_DIR}${INSTALL_PREFIX}/share/verilator/bin/.
