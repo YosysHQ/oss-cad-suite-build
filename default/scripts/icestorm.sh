e2402265fcb1db3c260ac215706ab95c8b647a67
Miodrag Milanovic <mmicko@gmail.com>
2021-06-10 13:35:42 +0200

    use file instead of symbolic link for  iceunpack

--- default/scripts/icestorm.sh
+++ default/scripts/icestorm.sh
@@ -4,6 +4,10 @@ if [ ${ARCH_BASE} != 'windows' ]; then
     mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec
     mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/icebox.py ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec/.
     mv ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/iceboxdb.py ${OUTPUT_DIR}${INSTALL_PREFIX}/libexec/.
+    for f in $(find ${OUTPUT_DIR}${INSTALL_PREFIX}/bin -type l)
+    do
+        cp --remove-destination $(readlink -e $f) $f
+    done
 else
     sed 's|#!/usr/bin/env python3|#|g' -i  ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/icebox.py
 fi
