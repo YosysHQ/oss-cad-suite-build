7d145c618d30e988504eff065a8faee8ea5d8879
Miodrag Milanovic <mmicko@gmail.com>
2021-11-08 09:11:28 +0100

    fix gtkwave build

--- default/scripts/gtkwave.sh
+++ default/scripts/gtkwave.sh
@@ -3,9 +3,12 @@ if [ ${ARCH_BASE} == 'darwin' ]; then
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.framework
 elif [ ${ARCH_BASE} == 'windows' ]; then
     cd gtkwave/gtkwave3-gtk3
+    sed -i -re "s,@GSETTINGS_RULES@,,g" ./src/Makefile.am
+    ./autogen.sh
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3 MINGW_LDADD="-lcomdlg32" --with-tcl=/usr/x86_64-w64-mingw32/sys-root/mingw/lib --with-tk=/usr/x86_64-w64-mingw32/sys-root/mingw/lib
 else
     cd gtkwave/gtkwave3-gtk3
+    ./autogen.sh
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3
 fi
 make DESTDIR=${OUTPUT_DIR} UPDATE_DESKTOP_DATABASE=/bin/true -j${NPROC} install
