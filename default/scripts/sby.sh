1252e3feb8943aad254ba03ce3f0fea652f1b480
Miodrag Milanovic <mmicko@gmail.com>
2022-06-30 17:57:37 +0200

    cleanup examples

--- default/scripts/sby.sh
+++ default/scripts/sby.sh
@@ -2,3 +2,5 @@ cd sby
 make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} -j${NPROC} install
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/examples
 cp -r docs/examples/* ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/.
+find ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/ -name "Makefile" -type f -delete
+find ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/ -name ".gitignore" -type f -delete
