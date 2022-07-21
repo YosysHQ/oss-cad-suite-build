e2557d42ef34607e78e1c229f29ef1ea8e664a57
Miodrag Milanovic <mmicko@gmail.com>
2022-07-21 11:40:46 +0200

    compile as c++

--- default/scripts/iceprogduino.sh
+++ default/scripts/iceprogduino.sh
@@ -2,9 +2,9 @@ mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/
 if [ ${ARCH} == 'windows-x64' ]; then
 	cd iceprogduino/windows/winiceprogduino
 	sed -i 's,usleep,usleep2,g' main.c
-	${CC} main.c -o iceprogduino.exe
+	${CXX} main.c -o iceprogduino.exe
 else
 	cd iceprogduino/programmer/iceprogduino
-	${CC} iceprogduino.c -o iceprogduino
+	${CXX} iceprogduino.c -o iceprogduino
 fi
 cp iceprogduino${EXE} ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/.
