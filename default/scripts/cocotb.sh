d70c259418b3f0b80529366cccc341f14f63ed19
Miodrag Milanovic <mmicko@gmail.com>
2022-08-24 12:00:58 +0200

    cleanup

--- default/scripts/cocotb.sh
+++ default/scripts/cocotb.sh
@@ -3,5 +3,6 @@ python3_package_setup
 sed -i -re "s,sys.platform == \"darwin\",sysconfig.get_platform() == \"darwin-x64\",g" cocotb/cocotb_build_libs.py
 pushd cocotb
 python3_package_install
+python3_package_pth "cocotb"
 python3_package_pip_install "pytest"
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/cocotb/share/makefiles/simulators/Makefile.verilator
