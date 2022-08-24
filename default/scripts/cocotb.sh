5997da9a7e9efcd62cc2996db1d5c1aec7d4beac
Miodrag Milanovic <mmicko@gmail.com>
2022-08-24 11:38:16 +0200

    Fix cocotb install script

--- default/scripts/cocotb.sh
+++ default/scripts/cocotb.sh
@@ -2,7 +2,6 @@ source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
 sed -i -re "s,sys.platform == \"darwin\",sysconfig.get_platform() == \"darwin-x64\",g" cocotb/cocotb_build_libs.py
 pushd cocotb
-python3_package_pip_install .
-mv ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/.
+python3_package_install
 python3_package_pip_install "pytest"
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/cocotb/share/makefiles/simulators/Makefile.verilator
