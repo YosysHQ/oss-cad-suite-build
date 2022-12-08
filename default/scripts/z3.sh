32d06d4a9a2578641a1d98d7511af6041a436417
Miodrag Milanovic <mmicko@gmail.com>
2022-12-08 15:36:03 +0100

    Fix Z3 for windows

--- default/scripts/z3.sh
+++ default/scripts/z3.sh
@@ -2,6 +2,8 @@ cd z3
 if [ ${ARCH_BASE} == 'windows' ]; then
 	sed -i 's,IS_LINUX=True,IS_MSYS2=True,g' scripts/mk_util.py
 	sed -i 's/sysname, _, _, _, machine = os.uname()/sysname,machine=[ "MINGW64", "x86_64"]/g' scripts/mk_util.py
+	sed -i 's,rewriter.h,rewriter_def.h,g' src/ast/rewriter/push_app_ite.h
+	sed -i 's,rewriter.h,rewriter_def.h,g' src/ast/rewriter/maximize_ac_sharing.h
 fi
 if [ ${ARCH_BASE} == 'darwin' ]; then
 	sed -i 's,IS_LINUX=True,IS_OSX=True,g' scripts/mk_util.py
