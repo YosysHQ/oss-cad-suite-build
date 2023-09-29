dd47698da0676a64a7ed7e9aae7657064c4d2b54
Miodrag Milanovic <mmicko@gmail.com>
2023-09-29 14:28:29 +0200

    install directly using git

--- default/scripts/pyhdl.sh
+++ default/scripts/pyhdl.sh
@@ -1,21 +1,7 @@
 source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
-python3_package_pip_install "pyyaml setuptools_scm"
-
-mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/pyhdl
-for target in *; do    
-    if [ $target != 'python3' ]; then
-        cp -r $target ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/pyhdl/.
-    fi
-done
-
-cd ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/pyhdl
-for target in *; do
-    pushd $target
-    python3_package_develop "--egg-path ./pyhdl/$target"
-    popd
-done
-( find . -type d -name ".git" && find . -name ".gitignore" && find . -name ".gitmodules" && find . -name ".gitattributes" && find . -name ".github" ) | xargs rm -rf
-
-python3_package_pth pyhdl
+python3_package_pip_install "git+https://github.com/amaranth-lang/amaranth"
+python3_package_pip_install "git+https://github.com/amaranth-lang/amaranth-boards"
+python3_package_pip_install "git+https://github.com/m-labs/migen"
+mv ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.8/site-packages/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/.
 
