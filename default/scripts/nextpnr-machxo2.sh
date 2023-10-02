2e60690c1c4d3dbb3ccb61896829d5f943b5b39e
Miodrag Milanovic <mmicko@gmail.com>
2023-10-02 08:47:58 +0200

    build for 1200 only, due to memory issues

--- default/scripts/nextpnr-machxo2.sh
+++ default/scripts/nextpnr-machxo2.sh
@@ -6,7 +6,7 @@ fi
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
       -DPython3_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.8 \
       -DPython3_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.8${SHARED_EXT} \
-      -DARCH=machxo2 \
+      -DARCH=machxo2 -DMACHXO2_DEVICES=1200 \
       -DMACHXO2_CHIPDB=${BUILD_DIR}/prjtrellis-bba/machxo2/chipdb \
       -DBUILD_GUI=${build_gui} -DUSE_IPO=OFF . -DBBA_IMPORT=${BUILD_DIR}/nextpnr-bba/nextpnr/bba/bba-export.cmake
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
