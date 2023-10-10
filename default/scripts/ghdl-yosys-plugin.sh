b3d626eb208e687bf413e522c839007fe1626418
Miodrag Milanovic <mmicko@gmail.com>
2023-10-10 13:04:20 +0200

    update ghdl plugin for darwin-x64

--- default/scripts/ghdl-yosys-plugin.sh
+++ default/scripts/ghdl-yosys-plugin.sh
@@ -2,7 +2,7 @@ cd ghdl-yosys-plugin
 sed -i 's,/yosyshq/share,/yosys/yosyshq/share,g' ../yosys/yosyshq/bin/yosys-config
 if [ ${ARCH} == 'darwin-x64' ]; then
     sed -i '11,13d' Makefile
-    make GHDL=../ghdl/yosyshq/bin/ghdl YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config CFLAGS="-I ../yosys/yosyshq/share/yosys/include" LIBGHDL_LIB="${BUILD_DIR}/ghdl${INSTALL_PREFIX}/lib/libghdl-3_0_0_dev.dylib" LIBGHDL_INC="${BUILD_DIR}/ghdl${INSTALL_PREFIX}/include/"
+    make GHDL=../ghdl/yosyshq/bin/ghdl YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config CFLAGS="-I ../yosys/yosyshq/share/yosys/include" LIBGHDL_LIB="${BUILD_DIR}/ghdl${INSTALL_PREFIX}/lib/libghdl-4_0_0_dev.dylib" LIBGHDL_INC="${BUILD_DIR}/ghdl${INSTALL_PREFIX}/include/"
 else
     make GHDL=../ghdl/yosyshq/bin/ghdl YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config CFLAGS="-I ../yosys/yosyshq/share/yosys/include"
 fi
