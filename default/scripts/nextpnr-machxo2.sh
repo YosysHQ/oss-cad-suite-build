8ad1e3c8f4df12fd4e2c297caa91c6ba63cb8113
Miodrag Milanovic <mmicko@gmail.com>
2023-10-19 08:23:50 +0200

    update python for nextpnr

--- default/scripts/nextpnr-machxo2.sh
+++ default/scripts/nextpnr-machxo2.sh
@@ -4,8 +4,8 @@ if [ ${ARCH} == 'linux-x64' ] || [ ${ARCH} == 'windows-x64' ] || [ ${ARCH} == 'd
       build_gui="ON"
 fi
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
-      -DPython3_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.8 \
-      -DPython3_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.8${SHARED_EXT} \
+      -DPython3_INCLUDE_DIR=${BUILD_DIR}/python3${INSTALL_PREFIX}/include/python3.11 \
+      -DPython3_LIBRARY=${BUILD_DIR}/python3${INSTALL_PREFIX}/lib/libpython3.11${SHARED_EXT} \
       -DARCH=machxo2 -DMACHXO2_DEVICES=1200 \
       -DMACHXO2_CHIPDB=${BUILD_DIR}/prjtrellis-bba/machxo2/chipdb \
       -DBUILD_GUI=${build_gui} -DUSE_IPO=OFF . -DBBA_IMPORT=${BUILD_DIR}/nextpnr-bba/nextpnr/bba/bba-export.cmake
