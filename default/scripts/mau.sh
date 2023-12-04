1507daeead62f0ec5a8c449ac041f6a4a543337b
Miodrag Milanovic <mmicko@gmail.com>
2023-12-04 12:09:37 +0100

    add colorama for windows

--- default/scripts/mau.sh
+++ default/scripts/mau.sh
@@ -1,6 +1,9 @@
 source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
 python3_package_pip_install "setuptools"
+if [ ${ARCH_BASE} == 'windows' ]; then
+    python3_package_pip_install "colorama"
+fi
 pushd mau
 python3_package_install
 python3_package_pth "mau"
