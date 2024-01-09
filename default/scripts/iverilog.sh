999fd1bc6520e5c5082997b5b95e3898869a1731
Miodrag Milanovic <mmicko@gmail.com>
2024-01-09 12:28:23 +0100

    fix iverilog for windows

--- default/scripts/iverilog.sh
+++ default/scripts/iverilog.sh
@@ -1,5 +1,13 @@
 cd iverilog
 sh autoconf.sh
+if [ ${ARCH} == 'windows-x64' ]; then
+    sed -i 's,@EXEEXT@,.exe,g' vvp/Makefile.in
+    sed -i 's,@EXEEXT@,.exe,g' ivlpp/Makefile.in
+    sed -i 's,@EXEEXT@,.exe,g' vhdlpp/Makefile.in
+    sed -i 's,@EXEEXT@,.exe,g' driver/Makefile.in
+    sed -i 's,@EXEEXT@,.exe,g' Makefile.in
+    sed -i 's,@EXEEXT@,.exe,g' driver-vpi/Makefile.in
+fi
 ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
 rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/include
