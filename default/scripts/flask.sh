36bb3fe7b03543e791483da52e33d2f1ff1f7485
Miodrag Milanovic <mmicko@gmail.com>
2024-01-10 11:33:31 +0100

    fix Werkzeug version

--- default/scripts/flask.sh
+++ default/scripts/flask.sh
@@ -1,5 +1,6 @@
 cd flask
 source ${PATCHES_DIR}/python3_package.sh
+sed -i 's,Werkzeug >= 2.0,Werkzeug == 2.3.7,g' setup.py
 python3_package_setup
 python3_package_install
 python3_package_pth "flask"
