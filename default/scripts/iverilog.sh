920375a1f6772f9515e85adc4170f745d9a25b80
Matt Taylor <mstaveleytaylor@gmail.com>
2024-01-12 01:33:17 +0000

    iverilog: re-add files under /include and /lib

--- default/scripts/iverilog.sh
+++ default/scripts/iverilog.sh
@@ -2,7 +2,5 @@ cd iverilog
 sh autoconf.sh
 ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME}
 make DESTDIR=${OUTPUT_DIR} -j${NPROC} install
-rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/include
-rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/*.a
 sed -i -re 's|^flag:VVP_EXECUTABLE=.*$||g' ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/ivl/vvp.conf
 sed -i -re 's|^flag:VVP_EXECUTABLE=.*$||g' ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/ivl/vvp-s.conf
