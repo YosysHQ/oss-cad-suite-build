e02c78c22c414121aff0b74a8f7a06db08dd895a
Miodrag Milanovic <mmicko@gmail.com>
2024-01-15 10:34:38 +0100

    remove flask as separate

--- default/rules/mcy.py
+++ default/rules/mcy.py
@@ -11,20 +11,6 @@ SourceLocation(
 Target(
 	name = 'mcy',
 	sources = [ 'mcy' ],
-	resources = [ 'flask' ],
-)
-
-SourceLocation(
-	name = 'flask',
-	vcs = 'git',
-	location = 'https://github.com/pallets/flask',
-	revision = 'tags/2.1.2',
-	license_file = 'LICENSE.rst',
-)
-
-Target(
-	name = 'flask',
-	sources = [ 'flask' ],
 	dependencies = [ 'python3', 'python3-native' ],
 	resources = [ 'python3'],
 )
--- default/scripts/flask.sh
+++
@@ -1,7 +0,0 @@
-cd flask
-source ${PATCHES_DIR}/python3_package.sh
-sed -i 's,Werkzeug >= 2.0,Werkzeug == 2.3.7,g' setup.py
-python3_package_setup
-python3_package_install
-python3_package_pth "flask"
-rm -rf ${OUTPUT_DIR}${INSTALL_PREFIX}/bin
