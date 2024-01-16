2e13b73a89c31a0ee0fbc3448b64ff90bad82552
Miodrag Milanovic <mmicko@gmail.com>
2024-01-16 11:40:55 +0100

    fix script

--- default/scripts/formal.sh
+++ default/scripts/formal.sh
@@ -2,6 +2,7 @@ source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
 # scy
 cd  ${BUILD_DIR}/scy
+sed -i 's,"yosys_mau,#"yosys_mau,g' pyproject.toml
 python3_package_pip_install "."
 mv ${OUTPUT_DIR}${INSTALL_PREFIX}/lib/python3.11/site-packages/bin ${OUTPUT_DIR}${INSTALL_PREFIX}/.
 #mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/examples/scy
