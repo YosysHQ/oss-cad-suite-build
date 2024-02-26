99c3e52e7b16292c5ccbdbc51ff0efdae64a99bf
Miodrag Milanovic <mmicko@gmail.com>
2024-02-26 15:46:20 +0100

    Update CXX for yosys build

--- default/scripts/yosys.sh
+++ default/scripts/yosys.sh
@@ -1,8 +1,7 @@
 cd yosys
 if  [ ${ARCH_BASE} == 'darwin' ]; then
 	make config-clang
-	sed -i -re "s,CXX = clang,CXX = ${CXX},g" Makefile
-	sed -i -re "s,LD = clang\+\+,LD = ${CXX},g" Makefile
+	sed -i -re "s,CXX = clang\+\+,CXX = ${CXX},g" Makefile
 elif [ ${ARCH} == 'linux-x64' ]; then
 	make config-clang
 elif [ ${ARCH} == 'windows-x64' ]; then
@@ -12,8 +11,7 @@ elif [ ${ARCH} == 'windows-x64' ]; then
 	sed 's|PYTHON := \$(shell cygpath -w -m \$(PREFIX)/bin/python3)|PYTHON := /usr/bin/python3|g' -i backends/smt2/Makefile.inc
 else
 	make config-gcc
-	sed -i -re "s,CXX = gcc,CXX = ${CC},g" Makefile
-	sed -i -re "s,LD = gcc,LD = ${CC},g" Makefile
+	sed -i -re "s,CXX = g++,CXX = ${CXX},g" Makefile
 fi
 make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} install -j${NPROC}
 pushd ${OUTPUT_DIR}${INSTALL_PREFIX} 
