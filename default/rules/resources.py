7d6d2c1a55fb5f756fc255d8eb3280799deb57d5
Yann Herklotz <git@yannherklotz.com>
2024-05-15 11:37:39 +0200

    Add environment file for fish shell support
    fixes #104

--- default/rules/resources.py
+++ default/rules/resources.py
@@ -3,13 +3,13 @@ from src.base import Target
 Target(
 	name = 'system-resources',
 	sources = [ ],
-	patches = [ 'fonts.conf.template', 'win-launcher.c', 'environment', 'environment.bat', 'start.bat', 'cacert.pem', 'tabbyadm' ],
+	patches = [ 'fonts.conf.template', 'win-launcher.c', 'environment', 'environment.fish', 'environment.bat', 'start.bat', 'cacert.pem', 'tabbyadm' ],
 	tools = {},
 )
 
 Target(
 	name = 'system-resources-tabby',
 	sources = [ ],
-	patches = [ 'fonts.conf.template', 'win-launcher.c', 'environment', 'environment.bat', 'start.bat', 'cacert.pem', 'tabbyadm' ],
+	patches = [ 'fonts.conf.template', 'win-launcher.c', 'environment', 'environment.fish', 'environment.bat', 'start.bat', 'cacert.pem', 'tabbyadm' ],
 	tools = {},
 )
--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -15,6 +15,9 @@ fi
 
 cp ${PATCHES_DIR}/${README} ${OUTPUT_DIR}${INSTALL_PREFIX}/README
 sed "s|___BRANDING___|${BRANDING}|g" -i ${OUTPUT_DIR}${INSTALL_PREFIX}/environment
+if [ -f "${OUTPUT_DIR}${INSTALL_PREFIX}/environment.fish" ]; then
+    sed "s|___BRANDING___|${BRANDING}|g" -i ${OUTPUT_DIR}${INSTALL_PREFIX}/environment.fish
+fi
 if [ -f "${OUTPUT_DIR}${INSTALL_PREFIX}/bin/tabbyadm" ]; then
     sed "s|___BRANDING___|${BRANDING}|g" -i ${OUTPUT_DIR}${INSTALL_PREFIX}/bin/tabbyadm
 fi
