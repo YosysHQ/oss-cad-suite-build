6181ad8c4a840396c6cbf504ad78f3d295f639a1
Miodrag Milanovic <mmicko@gmail.com>
2024-11-17 08:09:46 +0100

    make compilation of slang faster

--- default/scripts/yosys-slang-plugin.sh
+++ default/scripts/yosys-slang-plugin.sh
@@ -3,6 +3,6 @@ sed -i 's/Windows.h/windows.h/g' third_party/slang/source/util/OS.cpp
 sed -i 's,/yosyshq/share,../yosys/yosyshq/share,g' ../yosys/yosyshq/bin/yosys-config
 sed -i 's,/yosyshq/include,../yosys/yosyshq/share/yosys/include,g' ../yosys/yosyshq/bin/yosys-config
 sed -i 's,/yosyshq/lib,../yosys/yosyshq/lib,g' ../yosys/yosyshq/bin/yosys-config
-make YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config
+make YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config -j${NPROC}
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/share/yosys/plugins
 cp -rf build/slang.so ${OUTPUT_DIR}${INSTALL_PREFIX}/share/yosys/plugins/.
