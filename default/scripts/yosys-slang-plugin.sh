0fe54bca3e604f5800239bec7b5d6e54f02a95a6
Miodrag Milanovic <mmicko@gmail.com>
2025-04-02 17:36:01 +0200

    Update yosys-slang-plugin to build with CMake

--- default/scripts/yosys-slang-plugin.sh
+++ default/scripts/yosys-slang-plugin.sh
@@ -1,8 +1,8 @@
 cd yosys-slang-plugin
 sed -i 's/Windows.h/windows.h/g' third_party/slang/source/util/OS.cpp
-sed -i 's,/yosyshq/share,../yosys/yosyshq/share,g' ../yosys/yosyshq/bin/yosys-config
-sed -i 's,/yosyshq/include,../yosys/yosyshq/share/yosys/include,g' ../yosys/yosyshq/bin/yosys-config
-sed -i 's,/yosyshq/lib,../yosys/yosyshq/lib,g' ../yosys/yosyshq/bin/yosys-config
-make YOSYS_CONFIG=../yosys/yosyshq/bin/yosys-config -j${NPROC}
+sed -i 's,/yosyshq/share,${BUILD_DIR}/yosys/yosyshq/share,g' ../yosys/yosyshq/bin/yosys-config
+sed -i 's,/yosyshq/include,${BUILD_DIR}/yosys/yosyshq/share/yosys/include,g' ../yosys/yosyshq/bin/yosys-config
+sed -i 's,/yosyshq/lib,${BUILD_DIR}/yosys/yosyshq/lib,g' ../yosys/yosyshq/bin/yosys-config
+make YOSYS_PREFIX=../yosys/yosyshq/bin/ -j${NPROC}
 mkdir -p ${OUTPUT_DIR}${INSTALL_PREFIX}/share/yosys/plugins
 cp -rf build/slang.so ${OUTPUT_DIR}${INSTALL_PREFIX}/share/yosys/plugins/.
