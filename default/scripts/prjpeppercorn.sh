ead62af649f827ffddba74a034bb60ffd966f984
Miodrag Milanovic <mmicko@gmail.com>
2025-04-28 17:39:38 +0200

    skip this policy

--- default/scripts/prjpeppercorn.sh
+++ default/scripts/prjpeppercorn.sh
@@ -1,4 +1,5 @@
 cd prjpeppercorn/libgm
+sed -i 's,cmake_policy(SET CMP0167 NEW),,g' CMakeLists.txt
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
       .
