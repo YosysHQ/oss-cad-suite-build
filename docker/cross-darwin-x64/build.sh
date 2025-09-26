264c3cb9e4585fb0973043e51d069bca56d40ce5
metcalfinl <finlaymetcalfe@hotmail.com>
2025-09-26 14:45:42 +1200

    Feature: Update ubuntu version and bump docker container versions

--- docker/cross-darwin-x64/build.sh
+++ docker/cross-darwin-x64/build.sh
@@ -1,1 +1,1 @@
-docker build -t="yosyshq/cross-darwin-x64:2.2" -f Dockerfile.22 .
+docker build -t="yosyshq/cross-darwin-x64:3.0" -f Dockerfile.22 .
--- docker/cross-linux-x64/build.sh
+++ docker/cross-linux-x64/build.sh
@@ -1,1 +1,1 @@
-docker build -t="yosyshq/cross-linux-x64:2.2" -f Dockerfile.22 .
+docker build -t="yosyshq/cross-linux-x64:3.0" -f Dockerfile.22 .
