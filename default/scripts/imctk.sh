9bc13b3a9a2218866acb3fbeb4785926746911e8
metcalfinl <finlaymetcalfe@hotmail.com>
2025-09-30 18:38:15 +1300

    Fix: Added flags to ignore dangerous implicit autorefs, required by v1.88^

--- default/scripts/imctk.sh
+++ default/scripts/imctk.sh
@@ -2,4 +2,4 @@ cd imctk
 rm -rf abc-sys/codegen.sh
 sed -i 's,debug = true,#debug = true,g' Cargo.toml
 sed -i 's,pub const l_False,//pub const l_False,g' abc-sys/src/generated/bindings.rs
-cargo install --no-track --path eqy-engine --root ${OUTPUT_DIR}${INSTALL_PREFIX} --target=${CARGO_TARGET}
+RUSTFLAGS="-A dangerous-implicit-autorefs" cargo install --no-track --path eqy-engine --root ${OUTPUT_DIR}${INSTALL_PREFIX} --target=${CARGO_TARGET}
