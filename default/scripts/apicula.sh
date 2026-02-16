bb8ebbc5141677ee9a1aba1b1704e0013beda796
Miodrag Milanovic <mmicko@gmail.com>
2026-02-16 09:12:58 +0100

    add msgspec to apicula

--- default/scripts/apicula.sh
+++ default/scripts/apicula.sh
@@ -2,6 +2,7 @@ cd apicula
 source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
 python3_package_pip_install "crc"
+python3_package_pip_install "msgspec"
 curl -L https://github.com/YosysHQ/apicula/releases/download/0.0.0.dev/linux-x64-gowin-data.tgz > linux-x64-gowin-data.tgz
 tar xvfz linux-x64-gowin-data.tgz
 python3_package_install --old-and-unmanageable
