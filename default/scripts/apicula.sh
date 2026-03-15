46dabb1339de790fbb5fc35067cdc422044a4ba1
Pepijn de Vos <me@pepijndevos.nl>
2026-03-15 05:06:39 +0100

    Update apicula.sh dependencies

--- default/scripts/apicula.sh
+++ default/scripts/apicula.sh
@@ -1,8 +1,9 @@
 cd apicula
 source ${PATCHES_DIR}/python3_package.sh
 python3_package_setup
-python3_package_pip_install "crc"
-python3_package_pip_install "msgspec"
+python3_package_pip_install "fastcrc"
+python3_package_pip_install "msgpack"
+python3_package_install_numpy
 curl -L https://github.com/YosysHQ/apicula/releases/download/0.0.0.dev/linux-x64-gowin-data.tgz > linux-x64-gowin-data.tgz
 tar xvfz linux-x64-gowin-data.tgz
 python3_package_install --old-and-unmanageable
