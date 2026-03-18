0eb2dca5eaafa494ce866fd3d0b511b7cb1bb75c
Miodrag Milanovic <mmicko@gmail.com>
2026-03-18 13:47:15 +0100

    Update actions to @v5

--- src/base.py
+++ src/base.py
@@ -779,11 +779,11 @@ def generateYaml(target, build_arch, write_to_file):
 			yaml_content +="      - name: Get current date\n"
 			yaml_content +="        id: date\n"
 			yaml_content +="        run: echo \"date=$(date +'%Y-%m-%d')\" >> $GITHUB_OUTPUT\n"
-		yaml_content +="      - uses: actions/checkout@v4\n"
+		yaml_content +="      - uses: actions/checkout@v5\n"
 		if not target.top_package:
 			yaml_content +="      - name: Cache sources\n"
 			yaml_content +="        id: cache-sources\n"
-			yaml_content +="        uses: actions/cache@v4\n"
+			yaml_content +="        uses: actions/cache@v5\n"
 			yaml_content +="        with:\n"
 			yaml_content +="          path: _sources\n"
 			yaml_content +="          key: cache-sources-{}".format(target.name) + "\n"
