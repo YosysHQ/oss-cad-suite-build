42986d881b5620992a9bc1196fcb9c6ea03b76dd
Miodrag Milanovic <mmicko@gmail.com>
2026-06-03 12:51:40 +0200

    Switch to CMake builds

--- default/scripts/yosys.sh
+++ default/scripts/yosys.sh
@@ -1,19 +1,17 @@
 cd yosys
-if  [ ${ARCH_BASE} == 'darwin' ]; then
-	make config-clang
-	sed -i -re "s,CXX = clang\+\+,CXX = ${CXX},g" Makefile
-elif [ ${ARCH} == 'linux-x64' ]; then
-	make config-clang
-elif [ ${ARCH} == 'windows-x64' ]; then
-	echo 'CONFIG := msys2-64' > Makefile.conf
-	echo 'TCL_VERSION := tcl86' >> Makefile.conf
-	echo 'OS := MINGW' >> Makefile.conf
-	sed 's|PYTHON := \$(shell cygpath -w -m \$(PREFIX)/bin/python3)|PYTHON := /usr/bin/python3|g' -i backends/smt2/Makefile.inc
+if [ ${ARCH} == 'linux-x64' ]; then
+	echo 'set(CMAKE_C_COMPILER clang CACHE STRING "")' >> Configuration.cmake
+	echo 'set(CMAKE_CXX_COMPILER clang++ CACHE STRING "")' >> Configuration.cmake
 else
-	make config-gcc
-	sed -i -re "s,CXX = g\+\+,CXX = ${CXX},g" Makefile
+	touch Configuration.cmake
 fi
-make PREFIX=${INSTALL_PREFIX} DESTDIR=${OUTPUT_DIR} install -j${NPROC}
+cmake -C Configuration.cmake -B build . \
+	-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
+	-DCMAKE_BUILD_TYPE=Release
+cmake --build build -j${NPROC}
+DESTDIR=${OUTPUT_DIR} cmake --install build	--strip
+
 pushd ${OUTPUT_DIR}${INSTALL_PREFIX} 
 if [ ${ARCH} != 'windows-x64' ]; then
 	mkdir -p lib
