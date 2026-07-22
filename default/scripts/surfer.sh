8634e61c39d4283a134af452c79aaa9e56283fd1
Miodrag Milanovic <mmicko@gmail.com>
2026-07-22 11:06:33 +0200

    use stable

--- default/scripts/surfer.sh
+++ default/scripts/surfer.sh
@@ -1,2 +1,3 @@
+export RUSTUP_TOOLCHAIN=stable
 RUSTFLAGS="-C lto=off" cargo install --no-track --locked --root ${OUTPUT_DIR}${INSTALL_PREFIX} --target=${CARGO_TARGET} --path ./surfer/surfer
 cargo install --no-track --locked --root ${OUTPUT_DIR}${INSTALL_PREFIX} --target=${CARGO_TARGET} --path ./surfer/surver
