0c6f0582f45ab3d4053f32938d698996ab9256cd
Miodrag Milanovic <mmicko@gmail.com>
2026-07-23 14:24:32 +0200

    Fixes for windows

--- default/scripts/gtkwave.sh
+++ default/scripts/gtkwave.sh
@@ -5,6 +5,21 @@ if [ ${ARCH_BASE} == 'darwin' ]; then
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3 --with-tcl=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tcl.framework --with-tk=$(xcrun --show-sdk-path)/System/Library/Frameworks/Tk.framework --disable-dependency-tracking
 elif [ ${ARCH_BASE} == 'windows' ]; then
     sed -i -re "s,@GSETTINGS_RULES@,,g" ./src/Makefile.am
+    sed -i '55a\
+#if defined(__MINGW32__)\
+char *strndup(const char *s, size_t n)\
+{\
+    size_t len = strnlen(s, n);\
+    char *p = (char *)malloc(len + 1);\
+    if (!p)\
+        return NULL;\
+    memcpy(p, s, len);\
+    p[len] = '\''\0'\'';\
+    return p;\
+}\
+#endif\
+' src/helpers/vcd2fst.c
+    sed -i 's/extern int getopt ();/extern int getopt (int ___argc, char *const *___argv, const char *__shortopts);/' src/gnu-getopt.h
     ./autogen.sh
     ./configure --prefix=${INSTALL_PREFIX} --host=${CROSS_NAME} --enable-gtk3 MINGW_LDADD="-lcomdlg32" --with-tcl=/usr/x86_64-w64-mingw32/sys-root/mingw/lib --with-tk=/usr/x86_64-w64-mingw32/sys-root/mingw/lib
 else
