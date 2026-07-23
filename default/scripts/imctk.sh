478ac0b99493d44f215800a88b38f95e35782840
Miodrag Milanovic <mmicko@gmail.com>
2026-07-23 14:24:32 +0200

    Fixes for windows

--- default/scripts/imctk.sh
+++ default/scripts/imctk.sh
@@ -2,5 +2,11 @@ cd imctk
 rm -rf abc-sys/codegen.sh
 sed -i 's,debug = true,#debug = true,g' Cargo.toml
 sed -i 's,pub const l_False,//pub const l_False,g' abc-sys/src/generated/bindings.rs
+sed -i '$a\
+\
+#[cfg(target_os = "windows")]\
+#[link(name = "shlwapi")]\
+extern "system" {}\
+' abc-sys/src/lib.rs
 export RUSTUP_TOOLCHAIN=nightly
 RUSTFLAGS="-A dangerous-implicit-autorefs" cargo install --no-track --path eqy-engine --root ${OUTPUT_DIR}${INSTALL_PREFIX} --target=${CARGO_TARGET}
